c4297c99351f00ea7c2cf36522240a928a46e840
PVRX043 <stanislas.selle@laposte.fr>
2025-04-10 19:45:59 +0200

    delete game_logic.py

--- core/game_logic.py
+++
@@ -1,6 +0,0 @@
-# core/game_logic.py
-"""
-Contains the core game logic: Game state, Player, Vaisseau, Factions, Totems,
-turn management, and actions for a single-player high-score game.
-"""
-import game_state
